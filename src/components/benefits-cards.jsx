8cecd083faf8a9658b3f2df3dbab95ba35b6b7f4
Rankush <iamrankush@gmail.com>
2024-12-21 16:46:29 +0530

    animated added

--- src/components/benefits-cards.jsx
+++ src/components/benefits-cards.jsx
@@ -1,18 +1,25 @@
 import { FeatureCard } from "./benefit-card";
 import mobileCover from "../assets/img/mobile1.png";
 import mimg from "../assets/img/m1.avif";
+import { motion } from "framer-motion";
 
-export function Features({ features }) {
+export function Features({ features, childVariants }) {
   return (
     <>
       {/* left */}
-      <div className="w-full md:w-[45%] xl:w-[45%] grid grid-cols-1 gap-6">
+      <motion.div
+        variants={childVariants}
+        className="w-full md:w-[45%] xl:w-[45%] grid grid-cols-1 gap-6"
+      >
         <FeatureCard {...features[0]} />
         <FeatureCard {...features[2]} />
-      </div>
+      </motion.div>
 
       {/* center */}
-      <div className="w-full md:w-auto xl:w-auto order-first md:order-none xl:order-none">
+      <motion.div
+        variants={childVariants}
+        className="w-full md:w-auto xl:w-auto order-first md:order-none xl:order-none"
+      >
         <div className="relative w-[320px] h-[600px] mx-auto">
           <img src={mobileCover} alt="mobileCover" className="w-full h-full" />
           <img
@@ -22,13 +29,16 @@ export function Features({ features }) {
             src={mimg}
           />
         </div>
-      </div>
+      </motion.div>
 
       {/* right */}
-      <div className="w-full md:w-[45%] xl:w-[45%] grid grid-cols-1 gap-6">
+      <motion.div
+        variants={childVariants}
+        className="w-full md:w-[45%] xl:w-[45%] grid grid-cols-1 gap-6"
+      >
         <FeatureCard {...features[1]} />
         <FeatureCard {...features[3]} />
-      </div>
+      </motion.div>
     </>
   );
 }
